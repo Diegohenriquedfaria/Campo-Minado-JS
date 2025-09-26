334fea4bd3f081101be565e521a025d353ae2c0b
Diegohenriquedfaria <diego.9543884@aluno.mg.gov.br>
2025-09-26 17:18:32 -0300

    atualização

--- campo_minado.js
+++ campo_minado.js
@@ -2,8 +2,13 @@ document.addEventListener("DOMContentLoaded", function(){
 
     const grid = document.querySelector('.grid')
     const tamanhoEixo = 10 
-    const bandeirasFaltando = this.querySelector('#bandeira-faltando')
+    const bandeirasFaltando = document.querySelector('#bandeira-faltando')
+    const resultado = document.querySelector('#resultado');
     let quantiaBombas = 20 
+    let listaQuadrados = [];
+    let ehFimdejogo = false ; 
+
+
 
     function criarGrid(){
 
@@ -16,15 +21,80 @@ document.addEventListener("DOMContentLoaded", function(){
 
         for(let i = 0; i < tamanhoEixo * tamanhoEixo; i++){
 
-            const quadrado = document.createElement ('div')
-            quadrado.id = i 
-            quadrado.classList.add(listaEmbaralhada[i])
-            grid.appendChild(quadrado)
+            
+            const quadrado = document.createElement ('div');
+            quadrado.id = i ;
+            quadrado.classList.add(listaEmbaralhada[i]);
+            grid.appendChild(quadrado);
+            listaQuadrados.push(quadrado);
+            console.log(listaQuadrados);
+
+            quadrado.addEventListener('click',  function CliqueNormal (){
+
+                click(quadrado);
+            })
+
+            quadrado.addEventListener('contextmenu',  function cliqueBandeira (){
+
+             //   add.Bandeira(quadrado); 
+            })
 
         }
 
+        for(let i = 0 ; i  < listaQuadrados.length; i++){
 
+            let total = 0;
+            const lateralEsquerda = ( i % tamanhoEixo === 0 );
+            const lateralDireita = (i % tamanhoEixo === tamanhoEixo-1);
+            const lateralSuperior = ( i < tamanhoEixo);
+            const lateralInferior = (i >= tamanhoEixo * (tamanhoEixo - 1));
+
+
+            if (listaQuadrados[i].classList.contains('valido')){
+
+                if (!lateralSuperior && !lateralEsquerda && listaQuadrados[ i - tamanhoEixo -1].classList.contains('bomba')) total++;
+                if (!lateralSuperior && listaQuadrados[i - tamanhoEixo].classList.contains('bomba') ) total++;
+                if (!lateralSuperior && !lateralDireita  && listaQuadrados[ i - tamanhoEixo + 1].classList.contains('bomba')) total++;
+                if (!lateralEsquerda && listaQuadrados [ i -1].classList.contains('bomba')) total++;
+                if (!lateralDireita && listaQuadrados [ i+ 1].classList.contains('bomba')) total++;
+                if (!lateralInferior && !lateralEsquerda && listaQuadrados[i + tamanhoEixo - 1].classList.contains('bomba')) total++;
+                if (!lateralInferior && listaQuadrados[ i + tamanhoEixo].classList.contains('bomba') ) total++;
+                if (!lateralInferior && !lateralDireita && listaQuadrados[i + tamanhoEixo +1 ].classList.contains('bomba')) total++;
+
+                console.log(i)
+                console.log(total)
+
+                listaQuadrados[i].setAttribute('data' , total);
+            }
+        }
+    }
+    criarGrid() ; 
+
+    function click(quadrado) {
+        
+        if(ehFimdejogo || quadrado.classList.contains('checado') || quadrado.classList.contains('bandeira'))return;
+
+        if(quadrado.classList.contains('bomba')){
+            fimdejogo(quadrado);
+
+        }
+    }
+
+
+    function fimdejogo(quadrado){
+        resultado.innerHTML = ' BOOM! Fim de jogo . . . ' ; 
+        ehFimdejogo = true;
+
+        for(let i = 0 ; i  < listaQuadrados.length; i++){ 
+
+            if (listaQuadrados[i].classList.contains('bomba')){
+                listaQuadrados[i].innerHTML = '💣';
+                listaQuadrados[i].classList.remove('bomba');
+                listaQuadrados[i].classList.add('checado');
+            }
+        }
 
     }
-    criarGrid()
+   
+    
 })
